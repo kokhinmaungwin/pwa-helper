1b66559b5a5682cc54fe6ddbaf481dc8bdf831f9
kokhinmaungwin <khinmaungwin675@gmail.com>
2025-12-16 13:47:08 +0630

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "pwa-helper-v1.0.0";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./pwa-helper.js",
+  "./pwa-helper.min.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
